56f73c22f018717ff29432d607752891cb67384a
Laurens Verspeek <laurens_verspeek@hotmail.com>
2021-03-24 10:37:00 +0100

    make mail subscribe functionality

--- components/MailingList.vue
+++ components/MailingList.vue
@@ -7,14 +7,48 @@
       <h4 class="subtitle is-6">
         Get the latest news from across The Effect Network.
       </h4>
-      <div class="field has-addons is-justify-content-center">
-        <div class="control">
-          <input type="email" class="input is-white is-outlined" placeholder="Enter your email">
+      <form @submit.prevent="subscribe" v-if="!subscribed">
+        <div class="field has-addons is-justify-content-center">
+          <div class="control">
+            <input type="email" v-model="email" class="input is-white is-outlined"  placeholder="Enter your email">
+          </div>
+          <div class="control">
+            <button type="submit" class="button is-accent" :class="{'is-loading': loading}">Subscribe</button>
+          </div>
         </div>
-        <div class="control">
-          <a class="button is-accent">Subscribe</a>
-        </div>
+      </form>
+      <div v-else class="has-text-success subtitle has-text-weight-semibold">
+        Thanks for subscribing!
       </div>
     </div>
   </section>
 </template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'Subscribe',
+    data () {
+      return {
+        loading: false,
+        subscribed: false,
+        email: null
+      };
+    },
+    methods: {
+      subscribe: async function () {
+        if(this.email) {
+          this.loading = true
+          try {
+            await axios.post(`https://rosette-backend.rosette.ai/subscribe`, {email: this.email});
+            this.subscribed = true
+          } catch (e) {
+            //this.$modal.show({text: 'Could not subscribe, try again later or send an email to support@effect.ai', color: 'error'})
+            alert("Could not subscribe, try again later or send an email to support@effect.ai")
+          }
+          this.loading = false
+        }
+      }
+    }
+  };
+</script>
