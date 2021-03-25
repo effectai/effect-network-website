5b670438363d00967600bd24e694afc7a084823c
Laurens Verspeek <l.verspeek@gmail.com>
2021-03-25 18:19:07 +0100

    fix content develop blocks

--- components/DevelopBlocksAlt.vue
+++ components/DevelopBlocksAlt.vue
@@ -16,20 +16,17 @@
         <div class="card is-flat always-light p-5">
           <img src="~assets/img/icons/api.svg" style="height: 50px"/>
           <h3 class="subtitle is-4 mt-4 mb-2">API Documentation</h3>
-          <h4 class="subtitle is-5 mt-1">Explore Public Effect Repositories</h4>
-          <p class="block">Join the Effect.AI development effort by contributing to one of the open-source
-            projects on the public Effect repositories.
-          </p>
-          <a target="_blank" class="is-text is-outlined mt-2" :class="{'is-accent': $colorMode.value === 'dark'}" href="https://github.com/effectai">Explore Documentation</a>
+          <h4 class="subtitle is-5 mt-1">Effect Network API Documentation</h4>
+          <p class="block">Connect your applications to Effect Force to access a global human workforce or learn more about other dApps like EffectDAO.</p>
+          <a target="_blank" class="is-text is-outlined mt-2" :class="{'is-accent': $colorMode.value === 'dark'}" href="https://docs.effect.ai">Explore Documentation</a>
         </div>
       </div>
       <div class="column is-one-third">
         <div class="card is-flat always-light p-5">
           <img src="~assets/img/icons/whitepaper.svg" style="height: 50px"/>
-          <h3 class="subtitle is-4 mt-4 mb-2">Technical Whitepaper</h3>
-          <h4 class="subtitle is-5 mt-1">Explore Public Effect Repositories</h4>
-          <p class="block">Join the Effect.AI development effort by contributing to one of the open-source
-            projects on the public Effect repositories.
+          <h3 class="subtitle is-4 mt-4 mb-2">Effect Whitepaper</h3>
+          <h4 class="subtitle is-5 mt-1">Technical description</h4>
+          <p class="block">Read the original Effect Whitepaper for a technical and mathematical description of the Effect Network.
           </p>
           <a href="/download/effect_whitepaper.pdf" target="_blank" class="is-text is-outlined mt-2" :class="{'is-accent': $colorMode.value === 'dark'}" >Technical Whitepaper</a>
         </div>
