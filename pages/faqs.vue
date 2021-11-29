6245838399829cd4552bc4e75a8e047316f1cdc7
Noman Jabbar <nomanjabbar1999@gmail.com>
2021-11-29 17:44:14 +0100

    quickfixes

--- pages/faqs.vue
+++ pages/faqs.vue
@@ -2,7 +2,7 @@
   <section class="section has-navbar-fixed-top">
     <div class="container is-max-widescreen my-6 py-6">
       <h2 class="title is-2">Coming Soon</h2>
-      <p>For now, you can ask for help in our <a href="https://t.me/effectai" target="_blank">telegram channel</a></p>
+      <p>For now, you can ask for help in our <a href="https://t.me/effectaiannouncements" target="_blank">telegram channel</a></p>
     </div>
   </section>
 </template>
