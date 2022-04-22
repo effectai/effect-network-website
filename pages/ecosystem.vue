0e3332daf009422524225710aa61c3c9a52cf162
Jairzijp <jairzijp123@gmail.com>
2022-04-22 16:05:05 +0200

    WIP filters

--- pages/ecosystem.vue
+++ pages/ecosystem.vue
@@ -13,46 +13,64 @@
       </div>
     </section>
 
-      <!-- Featured dApps -->
-      <div class="featured-apps">
-        <div class="container pt-2 pb-6">
-          <h2 class="subtitle has-text-weight-semibold is-4 mb-5">Featured dApps</h2>
-          <div class="columns is-multiline">
-            <div
-              class="column is-full-mobile is-half-tablet is-one-fifth-desktop is-one-fifth-widescreen"
-              v-for="dapp in featuredDapps"
-              :key="dapp.id"
-            >
-              <nuxt-link to="/">
-                <div class="card is-shadowless has-background-white p-4">
-                  <div class="card-image mx-auto mb-2">
-                    <figure>
-                      <img :src="getImageLink(dapp.image_url)" v-bind:alt="dapp.image_url">
-                    </figure>
-                  </div>
-
-                  <div class="card-content p-2">
-                    <section class="title-section">
-                      <div class="media-content">
-                        <h2 class="title is-5 has-text-weight-medium mb-3 has-text-centered has-text-black">
-                          {{ dapp.group_name }}
-                        </h2>
-                        <div class="tags is-centered">
-                          <span v-for="tag in dapp.tags" :key="tag" class="tag is-info is-light">{{ tag }}</span>
-                        </div>
-                      </div>
-                    </section>
-                  </div>
+    <!-- Featured dApps -->
+    <div class="featured-apps">
+      <div class="container pt-2 pb-6">
+        <h2 class="subtitle has-text-weight-semibold is-4 mb-5">Featured dApps</h2>
+        <div class="columns is-multiline">
+          <div
+            class="column is-full-mobile is-half-tablet is-one-fifth-desktop is-one-fifth-widescreen"
+            v-for="dapp in featuredDapps"
+            :key="dapp.id"
+          >
+            <nuxt-link to="/">
+              <div class="card is-shadowless has-background-white p-4">
+                <div class="card-image mx-auto mb-2">
+                  <figure>
+                    <img :src="getImageLink(dapp.image_url)" v-bind:alt="dapp.image_url">
+                  </figure>
                 </div>
-              </nuxt-link>
-            </div>
+
+                <div class="card-content p-2">
+                  <section class="title-section">
+                    <div class="media-content">
+                      <h2 class="title is-5 has-text-weight-medium mb-3 has-text-centered has-text-black">
+                        {{ dapp.group_name }}
+                      </h2>
+                      <div class="tags is-centered">
+                        <span v-for="tag in dapp.tags" :key="tag" class="tag is-info is-light">{{ tag }}</span>
+                      </div>
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </nuxt-link>
           </div>
         </div>
       </div>
+    </div>
+
+    <!-- All dApps -->
     <section>
-      <!-- All dApps -->
       <div class="container my-6 py-6">
-        <div class="columns is-multiline">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <div class="tags is-align-items-center is-align-content-center is-flex">
+            <p class="is-size-7 mr-3" @click="filters = []">clear tags</p>
+            <span v-for="tag in filters" :key="tag" class="tag is-medium py-4 mb-0 is-info is-light">
+              {{ tag }}
+              <button @click="removeFilter(tag)" class="delete is-small"></button>
+            </span>
+          </div>
+          <div class="select">
+            <select>
+              <option>Sort by</option>
+              <option>Recently Added</option>
+              <option>Name A-Z</option>
+              <option>Name Z-A</option>
+            </select>
+          </div>
+        </div>
+        <div class="columns is-multiline mt-2">
           <div
             class="column is-full-mobile is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
             v-for="dapp in dappsList.submissions"
@@ -104,13 +122,10 @@ export default {
         return {
             example: null,
             dappsList: dapps,
+            filters: ['test', 'filter'],
             featuredDapps: dapps.submissions.filter(e => e.featured),
             publicPath: process.env.BASE_URL
         };
-    },
-    created() {
-      console.log(this.dappsList)
-      console.log(this.featuredDapps)
     },
     mounted() {
     },
@@ -123,6 +138,12 @@ export default {
     },
 
     methods: {
+      removeFilter (tag) {
+        this.filters = this.filters.filter(e => e !== tag)
+      },
+      addFilter (tag) {
+        this.filters.push(tag)
+      },
       getImageLink (img_url) {
         // /effect-network-website/static/img/ecosystem
         return require(`@/assets/img/ecosystem/${img_url}`)
@@ -131,6 +152,12 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.select select {
+  outline: none;
+  background-color: $light;
+  border: none;
+  color: #81889D;
+}
 .featured-apps {
   background: linear-gradient(0deg, $light 65%, transparent 35%);
 }
@@ -157,6 +184,19 @@ export default {
       object-fit: cover;
       border-radius: $card-radius !important;
     }
+  }
+}
+
+.tags {
+  .tag {
+    background: $light !important;
+    color: #81889D;
+  }
+  &.is-medium {
+    font-size: .95rem !important;
+  }
+  p {
+    cursor: pointer;
   }
 }
 </style>
