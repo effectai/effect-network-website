c01bf9fe5dd70eb2879d0bbc70abace6a1d33401
Jairzijp <jairzijp123@gmail.com>
2022-05-27 14:41:59 +0200

    add rings to globe

--- components/HomeBanner.vue
+++ components/HomeBanner.vue
@@ -18,11 +18,6 @@
 import ThreeGlobe from 'three-globe';
 import * as THREE from 'three';
 import TrackballControls from 'three-trackballcontrols';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
-import {
-  CSS2DRenderer
-} from "three/examples/jsm/renderers/CSS2DRenderer";
 
 export default {
   data() {
@@ -33,47 +28,63 @@ export default {
   components: {
   },
   mounted() {
-    // Add random markers
-    // TODO: make a set of predefined markers
-    const N = 60;
-    const gData = [...Array(N).keys()].map(() => ({
-      lat: ((Math.random() - 0.5) * 180),
-      lng: (Math.random() - 0.5) * 360,
-      size: 60,
-    }));
+    const points = [
+      [52.377956, 4.897070],
+      [41.385063, 2.173404],
+      [40.712776, -74.005974],
+      [-22.908333, -43.196388],
+      [37.835857, -122.586814],
+      [-34.2166866, 151.255740],
+      [25.0757595, 37.2977663],
+      [12.1202161, 15.0224395],
+      [-33.4727092, -70.7699147],
+      [53.5557125, -113.6328035],
+      [19.39068, -99.3055288],
+      [32.8829357, 13.1708263],
+      [18.0671579, -16.0236009],
+      [27.2306608, 2.5406943],
+      [44.4378043, 26.0245981],
+      [18.0671579, -16.0236009],
+      [-15.4168189, 28.273713],
+      [28.5273342, 77.1389452],
+      [47.8915649, 106.7617896],
+      [30.6584534, 103.9354627],
+      [-6.2297419, 106.7594779]
+    ]
+    const N = points.length;
+    const gData = [];
+    for (let i = 0; i < points.length; i++) {
+      gData.push({
+        lat: points[i][0],
+        lng: points[i][1],
+        maxR: 3,
+        propagationSpeed: .7,
+        repeatPeriod: Math.floor(Math.random() * (1100 - 900 + 1)) + 900
+      });
+    }
 
-    let cameraRotationSpeed = 0.0003;
- 
-    const world = new ThreeGlobe()
-      .globeImageUrl(require('@/assets/img/maps/Map_2.jpg'))
+
+    const colorInterpolator = t => `rgba(81, 88, 250,${1-t})`;
+    const cameraRotationSpeed = 0.0003
+
+    const Globe = new ThreeGlobe()
+      .globeImageUrl(require('@/assets/img/maps/white.jpg'))
       .atmosphereColor('#101D56')
       .atmosphereAltitude(0.05)
-      // .htmlElementsData(gData)
-      // .htmlElement(d => {
-      //   const el = document.createElement('div');
-      //   el.innerHTML = `<img src="${this.markerSvg}">`;
-      //   el.style.color = d.color;
-      //   el.style.width = `${d.size}px`;
-      //   el.style['pointer-events'] = 'auto';
-      //   return el;
-      // });
+      .ringsData(gData)
+      .ringColor(() => colorInterpolator)
+      .ringMaxRadius('maxR')
+      .ringPropagationSpeed('propagationSpeed')
+      .ringRepeatPeriod('repeatPeriod');
 
-    // Setup renderers
-    const renderers = [new THREE.WebGLRenderer(), new CSS2DRenderer()];
-    renderers.forEach((r, idx) => {
-      r.setSize(window.innerWidth * 1.5, window.innerHeight * 1.5);
-      if (idx > 0) {
-        // overlay additional on top of main renderer
-        r.domElement.style.position = 'absolute';
-        r.domElement.style.top = '0px';
-        r.domElement.style.pointerEvents = 'none';
-      }
-      document.getElementById('globe').appendChild(r.domElement);
-    });
+    // Setup renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth * 1.5, window.innerHeight * 1.5);
+    document.getElementById('globe').appendChild(renderer.domElement);
 
     // Setup scene
     const scene = new THREE.Scene();
-    scene.add(world);
+    scene.add(Globe);
     scene.add(new THREE.AmbientLight(0xffffff));
     scene.add(new THREE.DirectionalLight(0xffffff, 0.01));
     scene.background = null;
@@ -82,13 +93,13 @@ export default {
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 100;
     camera.position.x = 30;
-    camera.position.y = 0;
+    camera.position.y = 30;
     // camera.position.multiplyScalar(.1)
     camera.updateProjectionMatrix();
 
     // Add camera controls
-    const tbControls = new TrackballControls(camera, renderers[0].domElement);
-    tbControls.rotateSpeed = .5;
+    const tbControls = new TrackballControls(camera, renderer.domElement);
+    tbControls.rotateSpeed = 5;
     tbControls.noZoom = false;
     tbControls.minDistance = 275;
     tbControls.maxDistance = 275;
@@ -98,37 +109,15 @@ export default {
     // controls.enableZoom = false;
     // controls.rotateSpeed  = 0.1
 
-    renderers[0].setClearColor( 0x000000, 0 );
-
-    world.setPointOfView(camera.position, world.position);
-    tbControls.addEventListener('change', () => world.setPointOfView(camera.position, world.position));
+    renderer.setClearColor( 0x000000, 0 );
 
     (function animate() {
-      tbControls.update()
-      // controls.update()
-      scene.rotation.x += cameraRotationSpeed      
-      renderers.forEach(r => r.render(scene, camera));
-      //camera.lookAt(world.position)
-      world.setPointOfView(camera.position, world.position)
+      // Frame cycle
+      tbControls.update();
+      renderer.render(scene, camera);
+      scene.rotation.x -= cameraRotationSpeed
       requestAnimationFrame(animate);
     })();
-
-    window.addEventListener( 'resize', () => {
-      if (window.innerWidth < 1000) {
-        renderers.forEach(r => () => {
-          r.setSize(window.innerWidth, window.innerHeight);
-          r.render(scene, camera);
-        });
-      } else {
-        renderers.forEach(r => () => {
-          r.setSize(window.innerWidth * 1.5, window.innerHeight * 1.5);
-          r.render(scene, camera);
-        });
-      }
-      camera.updateProjectionMatrix();
-      world.setPointOfView(camera.position, world.position)
-      tbControls.update()
-    });
 
     
   }
