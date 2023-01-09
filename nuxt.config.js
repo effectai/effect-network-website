5b31991e2455fe3e1414428cc5d88beccf738c9e
David Britt <djmbritt@users.noreply.github.com>
2023-01-09 17:39:46 +0000

    add eos plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/aos', mode: 'client' }
+    { src: '~/plugins/aos', mode: 'client' },
+    { src: '~/plugins/eos', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
