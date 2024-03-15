68654a0edc33e6be52703b7fae0fc40767b0a55c
Laurens Verspeek <l.verspeek@gmail.com>
2024-03-15 11:28:20 +0100

    update logo

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,28 +1,13 @@
 <template>
-  <nav
-    class="navbar has-background-white pt-2 pb-1 is-sticky top-0"
-    role="navigation"
-    aria-label="main navigation"
-  >
+  <nav class="navbar has-background-white pt-2 pb-1 is-sticky top-0" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="effect-navbar is-flex">
         <nuxt-link class="" to="/">
-          <img
-            style="height: 3.5rem; width: auto"
-            src="/img/effect-logo.png"
-            alt="Effect Network"
-            class="logo p-1"
-          />
+          <img style="height: 3.5rem; width: auto" src="/img/effect-logo.svg" alt="Effect Network" class="logo p-1" />
         </nuxt-link>
 
-        <a
-          role="button"
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbar"
-          @click="mobileMenu = !mobileMenu"
-        >
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar"
+          @click="mobileMenu = !mobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -32,31 +17,19 @@
         <div class="navbar-start"></div>
         <div class="navbar-end is-align-items-center">
           <div @click="mobileMenu = false">
-            <nuxt-link
-              class="navbar-item"
-              to="/developers"
-              exact-active-class="is-active"
-            >
+            <nuxt-link class="navbar-item" to="/developers" exact-active-class="is-active">
               Developers
             </nuxt-link>
           </div>
           <div @click="mobileMenu = false">
-            <nuxt-link
-              to="/ecosystem"
-              class="navbar-item"
-              exact-active-class="is-active"
-            >
+            <nuxt-link to="/ecosystem" class="navbar-item" exact-active-class="is-active">
               Ecosystem
             </nuxt-link>
           </div>
           <div class="navbar-item" exact-active-class="is-active">
             <div @click="mobileMenu = false">
-              <a
-                href="https://app.effect.network"
-                target="_blank"
-                class="button is-outlined is-primary"
-                exact-active-class="is-active"
-              >
+              <a href="https://app.effect.network" target="_blank" class="button is-outlined is-primary"
+                exact-active-class="is-active">
                 Launch App
               </a>
             </div>
